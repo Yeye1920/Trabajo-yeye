4e5275ffb6be7b8e1f79c8fa94bbe1b8eed1b070
Yeye1920 <150769657+Yeye1920@users.noreply.github.com>
2023-11-14 00:11:10 -0500

    Add files via upload

---
+++ linda1.js
@@ -0,0 +1,19 @@
+
+   
+
+
+
+
+
+function IMAGEN() {
+	document.getElementById('L').src='hachi.PNG'
+	// body...
+}
+
+
+
+function IMAGEN2() {
+	document.getElementById('L').src='nana.PNG'
+	// body...
+}
+
